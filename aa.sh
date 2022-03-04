0236d1afe182cabec508a5d882b41cda4e5096b6
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2022-03-04 11:10:29 +0700

    Create aa.sh

---
+++ aa.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+cd /usr/bin
+
+wget -O add-host "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/add-host.sh"
+wget -O about "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/about.sh"
+wget -O usernew "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/member.sh"
+wget -O delete "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/delete.sh"
+wget -O cek "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/cek.sh"
+wget -O restart "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/info.sh"
+wget -O ram "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ram.sh"
+wget -O renew "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/renew.sh"
+wget -O autokill "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/tendang.sh"
+wget -O clear-log "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/clear-log.sh"
+wget -O change-port "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/change.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-ssl.sh"
+wget -O port-wg "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-wg.sh"
+wget -O port-tr "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-tr.sh"
+wget -O port-sstp "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-sstp.sh"
+wget -O port-squid "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-squid.sh"
+wget -O port-ws "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-ws.sh"
+wget -O port-vless "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/port-vless.sh"
+wget -O wbmn "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/xp.sh"
+wget -O swap "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/swapkvm.sh"
+wget -O menu "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/menu.sh"
+wget -O l2tp "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/l2tp.sh"
+wget -O ssh "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/ssh.sh"
+wget -O ssssr "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/ssssr.sh"
+wget -O sstpp "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/sstpp.sh"
+wget -O trojaan "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/trojaan.sh"
+wget -O v2raay "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/v2raay.sh"
+wget -O wgr "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/wgr.sh"
+wget -O vleess "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/vleess.sh"
+wget -O bbr "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/update/bbr.sh"
+wget -O autoreboot "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/autoreboot.sh"
+wget -O port-sslh "https://github.com/bokir-tampan/ranjau-darate/raw/main/port-sslh.sh"
+chmod +x add-host
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x member
+chmod +x delete
+chmod +x cek
+chmod +x restart
+chmod +x speedtest
+chmod +x info
+chmod +x ram
+chmod +x renew
+chmod +x about
+chmod +x autokill
+chmod +x ceklim
+chmod +x tendang
+chmod +x clear-log
+chmod +x change-port
+chmod +x port-ovpn
+chmod +x port-ssl
+chmod +x port-wg
+chmod +x port-tr
+chmod +x port-sstp
+chmod +x port-squid
+chmod +x port-ws
+chmod +x port-vless
+chmod +x wbmn
+chmod +x xp
+chmod +x swap
+chmod +x menu
+chmod +x l2tp
+chmod +x ssh
+chmod +x ssssr
+chmod +x sstpp
+chmod +x trojaan
+chmod +x v2raay
+chmod +x wgr
+chmod +x vleess
+chmod +x bbr
+chmod +x port-sslh
+
+cd 
+reboot
