ed406a25e9f6836f8edbf7a4b0b4bd80cf200975
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2022-03-04 22:04:40 +0700

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -4,7 +4,7 @@ cd /usr/bin
 
 wget -O add-host "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/add-host.sh"
 wget -O about "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/about.sh"
-wget -O usernew "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/usernew.sh"
+wget -O usernew "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/usernew.sh"
 wget -O trial "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/trial.sh"
 wget -O hapus "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/hapus.sh"
 wget -O member "https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/member.sh"
