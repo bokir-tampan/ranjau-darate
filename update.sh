d9cb8a28e2008156c4213558d803a924010e89f1
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2022-03-14 15:18:54 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /usr/bin
 wget -O usernew "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/usernew.sh"
+wget -O add-host "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/add-host.sh"
 cd
 echo "done"
