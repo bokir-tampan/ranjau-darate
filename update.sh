cc84d63269a97886a9fdb3a191c562631f08a15e
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2022-07-23 15:22:54 +0700

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /usr/bin
-wget -O usernew "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/usernew.sh"
-wget -O add-host "https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/add-host.sh"
-cd
-echo "done"
