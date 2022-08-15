93f21ca073702c8b37a41830ceadc73ce220417b
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2022-08-15 19:01:46 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 cd /usr/bin
-wget -q -O menu-vless "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-vless.sh"
-wget -q -O menu-vmess "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-vmess.sh"
-wget -q -O menu-socks "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-socks.sh"
-wget -q -O menu-bckp "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-bckp.sh"
-wget -q -O menu-ss "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-ss.sh"
-wget -q -O menu-trojan "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-trojan.sh"
-wget -q -O menu-ssh "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-ssh.sh"
-chmod +x menu-vless
-chmod +x menu-vmess
-chmod +x menu-ss
-chmod +x menu-socks
-chmod +x menu-trojan
-chmod +x menu-ssh
-chmod +x menu-bckp
+wget -q -O add-ssws "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/add-ssws.sh"
+chmod +x add-ssws
+cd 
+clear
+menu
