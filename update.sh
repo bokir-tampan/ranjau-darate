94beb9c9de044d5a4dc28c5544a3ef55a7627e17
bokir-tampan <moving.headers@gmail.com>
2022-10-26 05:08:49 +0000

    .

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /usr/bin
-wget -q -O menu-ssh "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-ssh.sh"
-wget -q -O menu-bckp "https://raw.githubusercontent.com/jsjsjsjsjsjsjsjsjsjsjsjs/hayomauapa/main/menu-bckp.sh"
-chmod +x menu-ssh
-chmod +x menu-bckp
-cd 
-clear
-menu
